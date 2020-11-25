4312374fccb4c8a8baf743ab1184ae7ef3651252
Alex-B-I <alex.berloh@gmail.com>
2020-11-24 19:36:13 -0600

    day2

---
+++ src/test/java/day02/GettingMoreInfoAboutResultSetObject.java
@@ -0,0 +1,39 @@
+package day02;
+
+import java.sql.*;
+
+public class GettingMoreInfoAboutResultSetObject {
+    public static void main(String[] args) throws SQLException {
+        String connectionStr = "jdbc:oracle:thin:@52.91.70.165:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        ResultSet rs   =   stmt.executeQuery("SELECT * FROM JOBS") ;
+
+        //Metadata-- data about the data --
+        //ResultSetMetaData -- data about the ResultSet object that contain our resulting rows and columns
+        // for example column names, column counts .. and more
+        //This is how we get the ResultSetMetaData object
+
+        ResultSetMetaData rsmd = rs.getMetaData();
+        //we only need two methods from this to get column count and column name I label
+
+        int colCount = rsmd.getColumnCount();
+        System.out.println("colCount = " + colCount);
+
+        //this is how we get column label I name using index
+        //get first column name
+        System.out.println("First Column Name is "+rsmd.getColumnLabel(1));
+        System.out.println("Second Column Name is "+rsmd.getColumnLabel(2));
+        System.out.println("===============================================");
+
+        // now print out all column names
+        for (int i = 1; i<=colCount;i++){
+            System.out.println(i +". Column Name is "+rsmd.getColumnLabel(i));
+        }
+
+
+    }
+}
---
+++ src/test/java/day02/Review.java
@@ -0,0 +1,66 @@
+package day02;
+
+import java.sql.*;
+
+public class Review {
+    public static void main(String[] args) throws SQLException {
+
+        String connectionStr = "jdbc:oracle:thin:@52.91.70.165:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs   =   stmt.executeQuery("SELECT * FROM JOBS") ;
+
+        // I want to read the first tow
+        rs.next();
+        System.out.println("First column value in Jobs: "+rs.getString(1));
+        System.out.println("Second column value in Jobs: "+rs.getString(2));
+        System.out.println("==========================================================");
+
+        //move to row number 7 and get above 2 columns values
+        rs.absolute(7);
+        System.out.println("First column value in Jobs row 7: "+rs.getString(1));
+        System.out.println("Second column value in Jobs row 7: "+rs.getString(2));
+        System.out.println("==========================================================");
+
+        rs.last();
+        System.out.println("First column value in Jobs last row: "+rs.getString(1));
+        System.out.println("Second column value in Jobs last row: "+rs.getString(2));
+        System.out.println("==========================================================");
+
+        rs.previous();
+        System.out.println("First column value in Jobs in 2nd from last row: "+rs.getString(1));
+        System.out.println("Second column value in Jobs in 2nd from last row: "+rs.getString(2));
+        System.out.println("==========================================================");
+
+        System.out.println("-------------------------Loop from first row till last row print JOB_ID-------------------------");
+
+        rs.beforeFirst();
+        while (rs.next()){
+            System.out.println("Loop First Column: "+rs.getString("JOB_ID"));
+        }
+
+        //Task#2
+        System.out.println("----------------------Loop from last row till first row get MIN_SALARY COLUMN AS NUMBER--------------------");
+        //WE are currently at after last location
+        //if you really want to make sure and explicitly say so
+        // we can do below
+        rs.afterLast();
+        while (rs.previous()){
+            //Nothing wrong with getting it as string, just for the sake of demo, we are getting as double
+            System.out.println("Min salary column as number:  $"+rs.getDouble("MIN_SALARY"));
+        }
+
+        //clean up the connection, statement and resultset object after usage
+
+        rs.close();
+        stmt.close();
+        conn.close();
+
+
+    }
+}
