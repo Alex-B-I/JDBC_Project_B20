22e7f3871598703246e27122576279eb2a8ee3a9
Alex-B-I <alex.berloh@gmail.com>
2020-11-25 20:53:30 -0600

    day3 added getRowMap

--- src/test/java/day3/UtilityPractice.java
+++ src/test/java/day3/UtilityPractice.java
@@ -2,7 +2,11 @@ package day3;
 
 import utility.DB_Utility;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class UtilityPractice {
     public static void main(String[] args) throws SQLException {
@@ -37,8 +41,31 @@ public class UtilityPractice {
 
         System.out.println("Job_title column value: "+DB_Utility.getColumnDataAsList("Job_title"));
 
+        System.out.println("===============================");
 
+        //        DB_Utility.displayAllData();
+
+        Map<String,String>  row1Map = new LinkedHashMap<>() ; //new HashMap<>() ;
+        //JOB_ID, JOB_TITLE, MIN_SALARY, MAX_SALARY
+        //AC_ACCOUNT   Public Accountant  4200   9000
+        row1Map.put("JOB_ID", "AC_ACCOUNT");
+        row1Map.put("JOB_TITLE", "Public Accountant");
+        row1Map.put("MIN_SALARY", "4200");
+        row1Map.put("MAX_SALARY", "9000");
+        // now do above programmatically
+        // create row 1 map like above programmatically
+//        System.out.println("row1Map = " + row1Map);
+
+        Map<String,String>  rowMap = new LinkedHashMap<>() ;
+        jobRS.absolute(1) ;
+        ResultSetMetaData rsmd = jobRS.getMetaData() ;
+        for (int colNum = 1; colNum <= rsmd.getColumnCount() ; colNum++) {
+            String columnName   =  rsmd.getColumnLabel( colNum ) ;
+            String cellValue    =  jobRS.getString( colNum ) ;
+            rowMap.put(columnName, cellValue) ;
+        }
+        System.out.println("first row rowMap = " + rowMap);
+        System.out.println("first row rowMap = " + DB_Utility.getRowMap(1));
         DB_Utility.destroy();
-
     }
 }
